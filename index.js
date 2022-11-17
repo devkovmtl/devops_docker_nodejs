0d7de02c70de9a2521241508cdeacd23af2981ba
Mikael <dev.kov.mtl@gmail.com>
2022-11-17 16:16:07 -0500

    Basic express setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8888;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2>Hello World</h2");
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
