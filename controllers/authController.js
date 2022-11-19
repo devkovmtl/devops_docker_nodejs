d59c38f36ed68c3a4034029c7340fe06cab8820a
Mikael <dev.kov.mtl@gmail.com>
2022-11-19 16:03:21 -0500

    Login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 
 exports.register = async (req, res, next) => {
   try {
-    console.log("REGISTER");
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -32,9 +31,53 @@ exports.register = async (req, res, next) => {
     return res.status(200).json({
       success: true,
       msg: "User created",
-      //   data: {
-      //     user: others,
-      //   },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      msg: "Server error",
+      data: null,
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+        data: null,
+      });
+    }
+    const user = await User.findOne({ username }).lean().exec();
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        msg: "Invalid credentials",
+        data: null,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        success: false,
+        msg: "Invalid credentials",
+        data: null,
+      });
+    }
+
+    const { password: pwd, __v, ...others } = user;
+
+    return res.status(200).json({
+      success: true,
+      msg: "OK",
+      data: {
+        user: others,
+      },
     });
   } catch (error) {
     return res.status(500).json({
