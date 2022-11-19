66997a6ad526ddc116805f59f72b38cc84323a75
Mikael <dev.kov.mtl@gmail.com>
2022-11-19 09:30:28 -0500

    Post model

---
+++ models/Post.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const postchema = new Schema(
+  {
+    title: { type: String, require: [true, "Post must have a title"] },
+    author: String,
+    body: { type: String, require: [true, "Post must have a body"] },
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+      votes: Number,
+      favs: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Post", postchema);
